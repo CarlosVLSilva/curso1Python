58b87a323a179e6a0ffea9fe1451a90eaebf21c5
Carlos Silva <carlosvlsilva15@gmail.com>
2024-07-12 15:29:44 -0300

    update

--- ex001.py
+++
@@ -1,5 +0,0 @@
-nome = input("Qual seu nome?  ")
-idade = int(input("Qual sua idade? "))
-altura = float(input("Qual sua altura? "))
-
-print(f'Olá, {nome}! Sua idade é {idade} anos. Sua altura é {altura} metros.')
