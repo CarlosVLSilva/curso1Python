a60f87c82c7817ace419376364c6924ee07228d2
Carlos Silva <159199472+CarlosVLSilva@users.noreply.github.com>
2024-07-16 15:53:08 -0300

    Create ex015.py

---
+++ ex015.py
@@ -0,0 +1,16 @@
+'''lista = [10, 20, 30, 40]
+lista.append(50)
+print(lista)
+removido = lista.pop(2)
+
+print(lista, removido)
+
+lista.insert(0, 60)
+print(lista[7])'''
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+print(lista_c)
+lista_a.extend(lista_b)
+print(lista_a)
