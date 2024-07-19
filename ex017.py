32cea47b5a2d419e44b0d4f09be6c8578e779116
Carlos Silva <159199472+CarlosVLSilva@users.noreply.github.com>
2024-07-18 22:39:33 -0300

    Create ex017.py
    Lista e Tupla

---
+++ ex017.py
@@ -0,0 +1,10 @@
+#Lista = mutável
+nomes = ['Maria', 'Helena', 'Luiz']
+nomes[1] = 'outro'
+_, _, nome = nomes
+print(nomes)
+print(nome)
+
+#Tupla = imutável 
+outros_nomes = 'Maria', 'Helena', 'Luiz'
+print(outros_nomes)
