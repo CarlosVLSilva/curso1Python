c1026c632274ebd50f19f727742576e10231b04d
Carlos Silva <159199472+CarlosVLSilva@users.noreply.github.com>
2024-07-31 16:49:12 -0300

    Create ex030.py

---
+++ ex030.py
@@ -0,0 +1,20 @@
+# Manipulando chaves e valores em dicionários 
+
+pessoa = {} # Dicionário vazio
+ 
+pessoa['nome'] = 'Luiz Otávio'
+ 
+print(pessoa)
+print(pessoa['nome'])
+
+
+
+# Chave Dinâmica
+
+pessoa = {}
+
+chave = 'nome_completo'
+
+pessoa[chave] = 'Luiz Otávio'
+
+print(pessoa[chave])
