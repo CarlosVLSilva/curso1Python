f354aabcc03cd6677e44c7beea0ca35467867133
Carlos Silva <159199472+CarlosVLSilva@users.noreply.github.com>
2024-08-27 10:31:48 -0300

    Create ex036.py

---
+++ ex036.py
@@ -0,0 +1,25 @@
+def executa(funcao, *args):
+    return funcao(*args)
+
+def soma(x, y):
+    return x + y
+    
+"""def cria_multiplicador(multiplicador):
+    def multiplica(numero):
+        return numero * multiplicador
+    return multiplica"""
+
+duplica = executa(
+    lambda m: lambda n: n * m,
+    2
+    )
+print(duplica(5))
+
+print(
+    executa(
+        lambda x, y: x + y,
+        2, 3
+        ),
+    executa(soma, 2, 3),
+    soma(2, 3)
+    )
